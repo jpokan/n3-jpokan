971361ea82754e53adcbb53a85a605c2b44ce2d2
jpokan <jorgeou1993@gmail.com>
2025-02-20 01:12:56 +0900

    fix: update lab

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="max-w-[502px] mx-auto pr-[104px] sm:pr-0 jpk-theme">
-		<p class="text-2xl pb-2 font-medium">Hello!</p>
+		<p class="text-2xl pb-2 font-medium dark:text-zinc-600">Hello!</p>
 		<p class="text-6xl pb-5 font-medium">I'm Jorge,</p>
-		<p class="text-2xl">I build websites and tools related to architecture and design</p>
+		<p class="text-2xl dark:text-zinc-400">I build websites and tools related to architecture and design</p>
 		<div class="py-10 text-center font-bold relative">
 			<button @click="show" class="jpk-link bg-zinc-200 dark:bg-zinc-800 w-10 h-10 rounded-full transition-none">
 				<span v-if="longbio">-</span>
@@ -12,13 +12,12 @@
 			</button>
 		</div>
 		<div class="pb-5 h-full" v-show="longbio">
-			<p class="dark:text-zinc-400" >I am</p>
+			<p class="pb-4 dark:text-zinc-400">
+			With a background in architecture, I am now into web development, combining design and technology to create functional digital experiences. Photography is one of my hobbies, allowing me to explore creativity in a different way.
+			</p>
 			<p class="font-semibold uppercase pb-4">Frontend web developer</p>
-			<p class="font-semibold uppercase pb-4">architect & designer</p>
-			<p class="font-semibold uppercase pb-4">Photographer</p>
-			<!-- <p class="pb-4 dark:text-zinc-400">Degree in architecture and design</p> -->
-			<p class="dark:text-zinc-400" >and work with</p>
-			<ul class="pb-4 font-semibold">
+			<p class="dark:text-zinc-400" >Stack/Tech</p>
+			<ul class="jpk-tools">
 				<li class="uppercase">
 					Nuxt / Vue / Tailwind / Threejs
 				</li>
@@ -26,13 +25,15 @@
 					Cloudflare / Netlify / Render
 				</li>
 			</ul>
-			<p class="dark:text-zinc-400">also</p>
-			<p class="pb-4 font-semibold uppercase">Blender / AutoCAD / Rhino / Revit /
+			<p class="font-semibold uppercase pb-4">architect & designer</p>
+			<p class="dark:text-zinc-400">Design and digital tools</p>
+			<p class="jpk-tools">Blender / AutoCAD / Rhino / Revit /
 				SketchUp / Photoshop / Illustrator / Darktable</p>
-			<p class="dark:text-zinc-400">and also</p>
-			<p class="pb-4 font-semibold">CANON R6MKII / 16-35mm 2.8f EF USMIII / 50mm EF 1.4f / 50mm EF 1.8f / 70-200mm EF 4.0f</p>
-			<p class="dark:text-zinc-400">Intersted in</p>
-			<p class="pb-4 uppercase font-semibold">creativity / tech / photography / travel / design / craft</p>
+			<p class="font-semibold uppercase pb-4">Photographer</p>
+			<p class="dark:text-zinc-400">Equipment</p>
+			<p class="jpk-tools">CANON R6MKII / 16-35mm 2.8f EF USMIII / 50mm EF 1.4f / 50mm EF 1.8f / 70-200mm EF 4.0f</p>
+			<p class="dark:text-zinc-400">Interests</p>
+			<p class="jpk-tools">creativity / tech / photography / travel / design / craft</p>
 			<p class="pb-4 dark:text-zinc-400">Need a craftsman with my skills? Send me a message.</p>
 			<p class="dark:text-zinc-400">I can speak spanish, english, cantonese. Currently learning japanese.</p>
 		</div>
--- pages/lab/index.vue
+++ pages/lab/index.vue
@@ -1,26 +1,38 @@
 <template>
 	<div class="sm:flex grid gap-5 relative">
 		<div class="sm:w-1/5">
-		<ContentNavigation v-slot="{navigation}" :query="query">
-		<div v-for="item in navigation[0].children" key="item">
-			<div class="border-b border-solid border-zinc-800 dark:border-zinc-200 font-semibold text-sm mb-5 uppercase">
-				{{ item.title }}
-			</div>
-			<ul class="mb-5">
-				<li v-for="i in item.children">
-					<!-- internal link -->
-					<NuxtLink class="jpk-link" v-if="i.i_url" :to="i.i_url">{{ i.title }}</NuxtLink>
-					<!-- external link -->
-					<!-- <a class="jpk-link relative" v-if="i.url" :href="i.url" target="_blank">{{ i.title }}<span class="text-xs absolute -right-4 top-0.5">↗</span></a> -->
-					<button @click="setURL(i.url)" class="jpk-link" v-if="i.url">{{ i.title }}</button>
-				</li>
-			</ul>
+			<ContentNavigation v-slot="{ navigation }" :query="query">
+				<div v-for="item in navigation[0].children" key="item">
+					<div
+						class="border-b border-solid border-zinc-800 dark:border-zinc-200 font-semibold text-sm mb-5 uppercase">
+						{{ item.title }}
+					</div>
+					<ul class="mb-5">
+						<li v-for="i in item.children">
+							<!-- internal link -->
+							<NuxtLink class="jpk-sublink" v-if="i.i_url" :to="i.i_url">{{ i.title }}</NuxtLink>
+							<!-- external link -->
+							<!-- <a class="jpk-link relative" v-if="i.url" :href="i.url" target="_blank">{{ i.title }}<span class="text-xs absolute -right-4 top-0.5">↗</span></a> -->
+							<button @click="setURL(i.url)" :class="{ active: isActive(i.url) }" class="jpk-sublink" v-if="i.url">{{ i.title }}</button>
+						</li>
+					</ul>
+				</div>
+			</ContentNavigation>
 		</div>
-		</ContentNavigation>
-		</div>
-		<div class="sm:w-4/5 sticky top-0 h-screen py-5">
-			<div class="border-zinc-800 border h-full">
-				<iframe :src="currentSite" frameborder="0" class="h-full w-full"></iframe>
+		<div class="sm:w-4/5 bottom-0 sticky sm:top-0 sm:h-screen h-96 py-5">
+			<div class="dark:border-zinc-200 border-zinc-800 border h-full w-full relative">
+				<div class="absolute inset-0 text-center -z-10">
+					<div class="h-full grid items-center">
+						<IconsLoader v-if="loading" class="w-12 mx-auto"/>
+						<span v-if="!currentSite" class="hidden sm:block">
+							Click links on the left to display preview
+						</span>
+						<span  v-if="!currentSite" class="sm:hidden">
+							Click links to display preview
+						</span>
+					</div>
+				</div>
+				<iframe v-show="!loading" :src="currentSite" @load="loaded" frameborder="0" class="h-full w-full bg-zinc-100 dark:bg-zinc-900"></iframe>
 			</div>
 		</div>
 	</div>
@@ -37,9 +49,18 @@ useHead({
 
 const query = queryContent('lab')
 const currentSite = ref('')
+const loading = ref(false)
 
-function setURL(arg){
+function loaded() {
+	loading.value = false
+	console.log('loaded');
+}
+const isActive = (arg)=>{
+	return currentSite.value === arg 
+}
+function setURL(arg) {
+	loading.value = true
 	currentSite.value = arg
 }
-</script>
 
+</script>
