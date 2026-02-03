610b5b26f1d4a017508b87fbe6506d95ddd20d42
jpokan <jorgeou1993@gmail.com>
2026-02-03 23:59:42 +0900

    fix: update theme color for gallery

--- components/Markdown/Gallery.vue
+++ components/Markdown/Gallery.vue
@@ -1,9 +1,9 @@
 <template>
-	<FancyBox class="grid grid-cols-2 gap-5">
+	<FancyBox :fancybox-options="options" class="grid grid-cols-2 gap-5">
 		<a
 			v-for="item in links"
 			:href="item"
-			:data-fancybox="`${project}-gallery`">
+			:data-fancybox="`gallery-${project ? project : 'main'}`">
 			<img
 				v-lazy="{ src: `${item}&tr=w-600` }"
 				class="object-cover w-full" />
@@ -14,4 +14,9 @@
 
 <script setup>
 const props = defineProps(["links", "project"]);
+const colorMode = useColorMode();
+
+const options = {
+	theme: colorMode.value,
+};
 </script>
